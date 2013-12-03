7ae720fee1ea2cae06b59017bdc70249627b1a36
Patrick Carey <patrick@rehabstudio.com>
2013-12-03 03:42:37 +0000

    greatly simplified script that sets up the environment for test runs

--- nacelle/test/environ.py
+++ nacelle/test/environ.py
@@ -1,4 +1,5 @@
 # stdlib imports
+import distutils.spawn
 import os
 import sys
 
@@ -15,23 +16,16 @@ def setup_environ():
     SDK sandbox
     """
 
-    # add the lib directory to the sytem path
+    # add nacelle's vendor directory to the sytem path
     sys.path.insert(0, os.path.join(ROOT_PATH, 'nacelle', 'vendor'))
 
     # Find the path on which the SDK is installed
-    sdk_path = None
-    for path in os.environ.get('PATH').split(os.pathsep):
-        if not os.path.exists(path):
-            continue
-        if 'dev_appserver.py' in os.listdir(path):
-            test_path = os.path.join(path, 'dev_appserver.py')
-            sdk_path = os.path.dirname(os.readlink(test_path) if os.path.islink(test_path) else test_path)
-            break
-
-    # crap out if we can't find the SDK
-    if not sdk_path:
-        sys.stderr.write("Fatal: Can't find sdk_path")
+    test_path = distutils.spawn.find_executable('dev_appserver.py')
+    if test_path is None:
+        print "ERROR: Can't find sppengine SDK on your PATH"
         sys.exit(1)
+    sdk_path = os.path.dirname(test_path)
+
     # add the SDK path to the system path
     sys.path.insert(0, sdk_path)
 
@@ -39,38 +33,7 @@ def setup_environ():
     from dev_appserver import fix_sys_path
     fix_sys_path()
 
-    from google.appengine.tools import dev_appserver as tools_dev_appserver
-    from google.appengine import dist
-
-    # Parse `app.yaml`
-    appinfo, url_matcher, from_cache = tools_dev_appserver.LoadAppConfig(
-        ROOT_PATH, {}, default_partition='dev')
-    app_id = appinfo.application
-
-    # Useful for later scripts
-    os.environ['APPLICATION_ID'] = app_id
-    os.environ['APPLICATION_VERSION'] = appinfo.version
-
-    # Add third-party libs to the system path
-    if appinfo.libraries:
-        for library in appinfo.libraries:
-            try:
-                dist.use_library(library.name, library.version)
-            except ValueError, e:
-                if library.name == 'django' and library.version == '1.5':
-                    # Work around an SDK issue
-                    # print 'Warning: django 1.5 not recognised by dist, fixing python path'
-                    sys.path.insert(0, os.path.join(sdk_path, 'lib', 'django-1.5'))
-                elif library.name == 'jinja2' and library.version == '2.6':
-                    sys.path.insert(0, os.path.join(sdk_path, 'lib', 'jinja2-2.6'))
-                elif library.name == 'webapp2' and library.version == '2.5.2':
-                    sys.path.insert(0, os.path.join(sdk_path, 'lib', 'webapp2-2.5.2'))
-                elif library.name == 'webob' and library.version == '1.2.3':
-                    sys.path.insert(0, os.path.join(sdk_path, 'lib', 'webob-1.2.3'))
-                elif library.name == 'PIL':
-                    try:
-                        from PIL import Image
-                    except ImportError:
-                        print 'Warning: Make sure you have PIL installed locally'
-                else:
-                    print 'Warning: Unsupported library:\n%s\n' % e
+    # add third-party libs to path
+    sys.path.insert(0, os.path.join(sdk_path, 'lib', 'jinja2-2.6'))
+    sys.path.insert(0, os.path.join(sdk_path, 'lib', 'webapp2-2.5.2'))
+    sys.path.insert(0, os.path.join(sdk_path, 'lib', 'webob-1.2.3'))
