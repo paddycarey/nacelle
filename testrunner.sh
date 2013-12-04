5055aa8b603e3d67a291461cc28ec351f0812338
Paddy Carey <patrick@rehabstudio.com>
2013-12-04 16:56:25 +0000

    added docstring to test runner

--- testrunner.sh
+++ testrunner.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Simple testrunner for nacelle using nose
+# Produces coverage report that can be pushed to services like coveralls.io
+#
 set -e
 
 coverage run --source="nacelle" --omit="google_appengine/*,nacelle/vendor/*" nacelle/test/runner.py
